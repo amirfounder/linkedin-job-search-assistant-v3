8c7c16ffe32255206250720bf66b4f873d4292c9
Amir Sharapov <amir@amirfounder.com>
2022-06-12 03:17:14 -0500

    updated status codes

--- src/api/companies.py
+++ src/api/companies.py
@@ -1,13 +1,14 @@
+from starlette import status
 from src.api.app import app
 from src.data.indices import company_index
 
 
-@app.get('/companies')
+@app.get('/companies', status_code=status.HTTP_200_OK)
 def get_companies():
     return [company for company in company_index.source.values()]
 
 
-@app.get('/companies/next')
+@app.get('/companies/next', status_code=status.HTTP_200_OK)
 def get_companies_to_target_next(return_count: int = 5):
     for name, company in company_index.items():
         pass
--- src/api/recruiters.py
+++ src/api/recruiters.py
@@ -1,22 +1,20 @@
 from .app import app
+from starlette import status
 from src.data.indices import recruiter_index
 from src.data.models import Recruiter
 
 
-@app.get('/recruiters')
+@app.get('/recruiters', status_code=status.HTTP_200_OK)
 def get_recruiters():
-    recruiters = []
-    for k, v in recruiter_index.source.items():
-        recruiters.append(v.dict())
-    return recruiters
+    return [recruiter.json() for recruiter in recruiter_index.values()]
 
 
-@app.get('/recruiters/{username}')
+@app.get('/recruiters/{username}', status_code=status.HTTP_200_OK)
 def get_recruiters_by_username(username: str):
     return recruiter_index.get(username)
 
 
-@app.post('/recruiters')
+@app.post('/recruiters', status_code=status.HTTP_201_CREATED)
 def post_recruiter(recruiter: Recruiter):
     recruiter_index[recruiter.username] = recruiter
-    return recruiter.dict()
+    return {recruiter.dict()}
